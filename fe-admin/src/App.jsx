582a3c09c045a96804b4c4f5b8bf6cc74e59a344
Huydzsss <144263815+Huydzsss@users.noreply.github.com>
2025-04-28 20:39:27 +0700

    chỉnh sửa fe-admin

--- fe-admin/src/App.jsx
+++ fe-admin/src/App.jsx
@@ -1,5 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // project imports
 import router from 'routes';
 import ThemeCustomization from 'themes';
@@ -14,6 +15,7 @@ export default function App() {
       <ScrollTop>
         <RouterProvider router={router} />
       </ScrollTop>
+      <ToastContainer position="top-right" autoClose={2000} />
     </ThemeCustomization>
   );
 }
