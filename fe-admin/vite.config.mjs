7fd1dc20482c93406213e927945548bbb8273266
Huydzsss <144263815+Huydzsss@users.noreply.github.com>
2025-06-27 02:06:49 +0700

    Update vite.config.mjs

--- fe-admin/vite.config.mjs
+++ fe-admin/vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
   const API_URL = `${env.VITE_APP_BASE_NAME}`;
   const PORT = 3003;
 
